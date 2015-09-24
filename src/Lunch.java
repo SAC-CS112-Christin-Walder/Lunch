83f602ba1af98f36434394d3255dc7d30c6de446
SAC-CS112-Christin-Walder <cdubs808@gmail.com>
2015-09-23 21:58:07 -0700

    Fourth

--- src/Lunch.java
+++ src/Lunch.java
@@ -8,21 +8,25 @@ public class Lunch
 		Scanner input = new Scanner(System.in);
 		
 		int thirsty;
+		int breakfast;
 		
 		System.out.println("You are hungry and enter the lunch room");
 		System.out.println("Get in line");
 		System.out.println("Buy lunch");
 		
-		System.out.println("Are you thirsty? (1 for yes or 0 for no)");
-		thirsty=input.nextInt();
+		System.out.println("Are you thirsty? (1 for yes, 0 for no)");
+			thirsty=input.nextInt();
 		
 		if (thirsty==1){
-			System.out.println("Buy Coke");}
-		else if (thirsty==0){
-			System.out.println("Get water");}
-		else {
-			System.out.println("You don't get a drink");
-		}
+			System.out.println("Did you eat breakfast? (1 for yes, 0 for no)");
+			breakfast=input.nextInt();
+			if (breakfast==1){
+				System.out.println("Buy Coke");}
+				else { 
+					System.out.println("Buy Diet Coke");} }
+			else {
+				System.out.println("You don't get a drink");}
+
 		
 		System.out.println("Eat lunch");
 		System.out.println("Return tray");
